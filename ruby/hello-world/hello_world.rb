26d64b41b0b4134bc97bec3858388d027a653906
efgalvao <solfieri@hotmail.com>
2020-10-15 17:20:38 -0300

    Hello World done.

--- ruby/hello-world/hello_world.rb
+++ ruby/hello-world/hello_world.rb
@@ -6,3 +6,14 @@ To get started with TDD, see the `README.md` file in your
 `ruby/hello-world` directory.
 =end
 
+class HelloWorld
+    def self.hello
+        "Hello, World!"
+    end
+end
+# class HelloWorld
+
+# def hello
+#     puts "Hello, World!"
+# end
+# end
