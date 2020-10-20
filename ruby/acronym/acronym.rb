474beff273fadc710f52780703a05be96dca42bc
efgalvao <solfieri@hotmail.com>
2020-10-19 21:03:22 -0300

    Acronym ok

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -6,3 +6,14 @@ To get started with TDD, see the `README.md` file in your
 `ruby/acronym` directory.
 =end
 
+class Acronym
+    def self.abbreviate(phrase)
+        initials = ""
+        phrase.split(/[ -]/).each do |n|
+            if n[0] != " " and n[0] != nil
+            initials += n[0].capitalize
+            end
+        end
+        initials
+    end
+end
