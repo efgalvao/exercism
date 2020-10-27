d4f6b41acbd1c3992cc197456212c9ac9a7122fd
efgalvao <solfieri@hotmail.com>
2020-10-27 11:44:17 -0300

    Raindrops done.

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -5,4 +5,25 @@ Write your code for the 'Raindrops' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/raindrops` directory.
 =end
-
+class Raindrops
+    def self.convert(x)
+      result = ""
+        a = (1..x).select { |n|x % n == 0}
+      a.each do |n|
+        if n.to_i == 3
+        result += 'Pling'
+        end
+        if n.to_i == 5
+        result += 'Plang'
+        end
+        if n.to_i == 7
+        result += 'Plong'
+        end
+        end
+        if result.empty?
+            x.to_s
+        else
+            result
+        end
+    end
+end
