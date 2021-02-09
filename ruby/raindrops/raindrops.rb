8a64b27f4001c45d5a7b556f9ef28ec63cb27ee4
edmar.galvao@codeminer42.com <solfieri@hotmail.com>
2021-02-09 08:37:59 -0300

    Update raindrops

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,29 +1,10 @@
-=begin
-Write your code for the 'Raindrops' exercise in this file. Make the tests in
-`raindrops_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/raindrops` directory.
-=end
 class Raindrops
-    def self.convert(x)
-      result = ""
-        a = (1..x).select { |n|x % n == 0}
-      a.each do |n|
-        if n.to_i == 3
-        result += 'Pling'
-        end
-        if n.to_i == 5
-        result += 'Plang'
-        end
-        if n.to_i == 7
-        result += 'Plong'
-        end
-        end
-        if result.empty?
-            x.to_s
-        else
-            result
-        end
-    end
+  def self.convert(droplet)
+    result = ''
+    result << 'Pling' if (droplet % 3).zero?
+    result << 'Plang' if (droplet % 5).zero?
+    result << 'Plong' if (droplet % 7).zero?
+    result << droplet.to_s if result.empty?
+    result
+  end
 end
