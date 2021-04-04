49bc8935f4ba27b9cb27a317cb3d1531e67f0c5b
efgalvao <solfieri@hotmail.com>
2021-04-04 13:17:04 -0300

    Resolves sieve exercise.

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -1,8 +1,19 @@
-=begin
-Write your code for the 'Sieve' exercise in this file. Make the tests in
-`sieve_test.rb` pass.
+class Sieve
+    def initialize(limit)
+      @limit = limit
+    end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/sieve` directory.
-=end
+    def primes
+        numbers_list = Array(2..@limit)
+        numbers_list.each do |p|
+            i = p
+            while (p * i) <= @limit
+                multiple = p * i
+                numbers_list.delete(multiple)
+                i += 1
+            end
+            numbers_list
+        end
 
+    end
+end
