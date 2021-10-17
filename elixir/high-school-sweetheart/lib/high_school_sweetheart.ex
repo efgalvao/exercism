191ae76119cfcde72895e988381e9c67058a19a0
efgalvao <solfieri@hotmail.com>
2021-10-17 15:55:24 -0300

    Elixir - high_school_sweetheart exerccise

---
+++ elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -0,0 +1,35 @@
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    String.at(String.trim(name), 0)
+  end
+
+  def initial(name) do
+    String.upcase(first_letter(name)) <> "."
+  end
+
+  def initials(full_name) do
+    names = String.split(full_name, " ", trim: true)
+    initials = Enum.map(names, fn name -> String.upcase(initial(name)) end)
+    # initials = initial
+    Enum.join(initials, " ")
+  end
+
+  def pair(full_name1, full_name2) do
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{initials(full_name1)}  +  #{initials(full_name2)}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
+  end
+end
