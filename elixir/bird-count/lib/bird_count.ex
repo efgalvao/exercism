861a1a0344ca014bb02e6d7def24af58f0910e02
efgalvao <solfieri@hotmail.com>
2021-10-24 22:54:51 -0300

    Elixir - Bird count

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,16 +1,12 @@
 defmodule BirdCount do
-  def today(list) when list == [] do
-    nil
-  end
+  def today(list) when list == [], do: nil
   
   def today(list) do
   [h | _] = list
     h
   end
 
-  def increment_day_count(list) when list == [] do
-    [1]
-  end
+  def increment_day_count(list) when list == [], do: [1]
 
   def increment_day_count(list) do
     [h | t] = list
@@ -18,9 +14,7 @@ defmodule BirdCount do
     [h | t]
   end
 
-  def has_day_without_birds?(list) when list == [] do
-    false  
-  end
+  def has_day_without_birds?(list) when list == [], do: false
 
   def has_day_without_birds?(list) do
     [h | t] = list
@@ -30,27 +24,14 @@ defmodule BirdCount do
     end    
   end
 
-  def total([]) do
-    0
-  end
+  def total([]), do: 0
 
   def total(list) do
     [h | t ] = list
     h + total(t)
   end
 
-  def busy_days([]) do
-    0
-  end
-
-  def busy_days(list) do
-    counter = 0
-    [h | t] = list
-    if h > 4 do
-      counter + 1
-    else
-      counter + busy_days(t)
-    end
-    counter
-  end
+  def busy_days([]), do: 0
+  def busy_days([head | tail]) when head >= 5, do: busy_days(tail) + 1
+  def busy_days([_ | tail]), do: busy_days(tail)
 end
