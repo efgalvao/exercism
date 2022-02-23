e155945e0f89706143b6840685a594da4472e4ed
efgalvao <solfieri@hotmail.com>
2022-02-22 22:24:04 -0300

    ruby/tournament complete

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -1,51 +1,77 @@
+# frozen_string_literal: true
+
+HEADER = "Team                           | MP |  W |  D |  L |  P\n"
 
 class Tournament
+  def self.tally(input)
+    return HEADER if input.length < 5
 
-	def self.tally(input)
-		r = "Team                           | MP |  W |  D |  L |  P\n"			
-		if input.length > 10
-			# print input.class
-			input = input.split(";").map! do |n| 
-			n = [n]
-			n <<  {:played => 0, :wins => 0, :draws => 0, :losses => 0, :points => 0}
-			end
-			c = self.calculate(input)
-			c.each do |n|
-			# puts n
-			r << "#{n[0]} #{" " * (31 - n[0].length)}|  #{n[1][:played]} |  #{n[1][:wins]} |  #{n[1][:draws]} | #{n[1][:losses]}  |  #{n[1][:points]}\n"
-			end
-		end
-		r
-	end 
+    input = input.split("\n")
+    table = {}
 
-	private
+    input.each do |match|
+      result_table = calculate_match(match)
+      table = sum_tables(table, result_table)
+    end
+    result = HEADER.to_s
+    ordered_table = table.sort_by { |k, v| [-v[:points], k] }
+    ordered_table.each do |n|
+      result += "#{n[0]} #{' ' * (30 - n[0].length)}|  #{n[1][:played]} |  #{n[1][:wins]} |  #{n[1][:draws]} |  #{n[1][:losses]} |  #{n[1][:points]}\n"
+    end
+    result
+  end
 
-def self.calculate(table)
-	if table[2][0] == "win"
-		table[0][1][:played] += 1
-		table[0][1][:wins] += 1
-		table[0][1][:points] +=  3
-		table[1][1][:played] += 1 
-		table[1][1][:losses] += 1
-	end 
-	if table[2] == "loss"
-		table[0][:played] += 1 
-		table[0][:losses] += 1
-		table[1][:played] += 1 
-		table[1][:win] += 1
-		table[1][:points] +=  3
-	end 
-	if table[2] == "draw"
-		table[0][:played] += 1 
-		table[1][:played] += 1 
-		table[0][:draws] += 1
-		table[1][:draws] += 1
-		table[0][:points] += 1
-		table[1][:points] += 1
-	end
-	table.delete_at(2)
-	return table
-end 
+  def self.sum_tables(table1, table2)
+    table = table1.dup
+    table2.each do |team, score|
+      table[team] = {} if table[team].nil?
+      table1[team] = {} if table1[team].nil?
+      table[team] = table1[team].merge(score) { |_k, b_value, a_value| a_value + b_value }
+    end
+    table
+  end
+
+  def self.win(table, winner)
+    table[winner][:played] += 1
+    table[winner][:wins] += 1
+    table[winner][:points] += 3
+  end
+
+  def self.loss(table, loser)
+    table[loser][:played] += 1
+    table[loser][:losses] += 1
+  end
+
+  def self.draw(table, team1, team2)
+    table[team1][:played] += 1
+    table[team2][:played] += 1
+    table[team1][:draws] += 1
+    table[team2][:draws] += 1
+    table[team1][:points] += 1
+    table[team2][:points] += 1
+  end
+
+  def self.calculate_match(match)
+    table = {}
+    match = match.split(';').map! do |n|
+      if n.length <= 5
+        n.chomp
+      else
+        table[n] = { played: 0, wins: 0, draws: 0, losses: 0, points: 0 }
+        n
+      end
+    end
+
+    case match[2]
+    when 'win'
+      win(table, match[0])
+      loss(table, match[1])
+    when 'loss'
+      win(table, match[1])
+      loss(table, match[0])
+    else
+      draw(table, match[0], match[1])
+    end
+    table
+  end
 end
-
-# Tournament.tally("Allegoric Alaskans;Blithering Badgers;win")
