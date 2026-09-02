78b9afbaca487c964b63561f618f2eeba8b82a31
efgalvao <solfieri@hotmail.com>
2026-09-01 22:21:47 -0300

    Add initial implementation of cars assemble exercise with HELP, HINTS, README, and test files

---
+++ go/cars-assemble/cars_assemble.go
@@ -0,0 +1,24 @@
+package cars
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour.
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return float64(productionRate) * (successRate / 100)
+	panic("CalculateWorkingCarsPerHour not implemented")
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate) / 60)
+	panic("CalculateWorkingCarsPerMinute not implemented")
+}
+
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	var complete int = carsCount / 10
+	var remainder int = carsCount % 10
+
+	return uint(complete*95000 + remainder*10000)
+	panic("CalculateCost not implemented")
+}
